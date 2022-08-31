f714b4645baf1249d231d052b86a69b7061eab01
HarmeetKaur7 <harmeet.kaur@freshworks.com>
2022-08-31 20:24:41 +0530

    Added Validations in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    
+    validates :username ,presence: true,
+              uniqueness: {case_sensitive: false} ,
+              length:{minimum:3,maximum:25}
+    REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,presence: true, 
+              uniqueness: {case_sensitive: false},
+              length:{maximum:50},
+              format:{with: REGEX_EMAIL}
+   
 end
