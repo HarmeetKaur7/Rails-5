82c7182624409048c9c3090625089734baef1594
HarmeetKaur7 <harmeet.kaur@freshworks.com>
2022-08-31 23:25:58 +0530

    Added User action Create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
     def new
         @user=User.new
     end
+
+    def create
+        @user=User.new(params_user)
+        if @user.save
+            flash[:notice] = "Welcome #{@user.username}, You have successfully signedup !"
+            redirect_to articles_path
+        else
+            render 'new'   
+        end 
+    end
+
+    private
+    def find_user
+        @user=User.find(params[:id])
+    end
+    def params_user
+        params.require(:user).permit(:username,:email,:password)
+    end
+
 end
