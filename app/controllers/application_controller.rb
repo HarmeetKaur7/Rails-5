da5a5e874ce40f1a7427bbe22332ce449fe72da4
HarmeetKaur7 <harmeet.kaur@freshworks.com>
2022-09-01 18:25:21 +0530

    Restricted actions from UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
    if session[:user_id]
     @current_user ||= User.find(session[:user_id]) 
     end
+  end
 
     def logged_in?
         !!current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
         user= User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id]=user.id
-            flash[:notice]="login Successful ! "
+            flash[:notice]="login Successful #{user.username} ! "
             redirect_to user_path(user)
         else
-            flash.now[:alert]= "Wrong Credentials !" #doesnt wait for one full request cycle
+            flash.now[:alert]= "Wrong Credentials  !" #doesnt wait for one full request cycle
             render 'new'
         end
     end
 
     def destroy
         session[:user_id]=nil
-        flash[:notice]="logged out ! "
+        flash[:notice]="logged out  ! "
         redirect_to root_path
     end
 
