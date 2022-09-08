cab53085fc1df4ca03f7575d9ae250f15fc7137b
HarmeetKaur7 <harmeet.kaur@freshworks.com>
2022-09-08 14:18:17 +0530

    configured Mailer

--- app/jobs/sending_articles_to_users_job.rb
+++ app/jobs/sending_articles_to_users_job.rb
@@ -1,7 +1,8 @@
 class SendingArticlesToUsersJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    "Do something later"
+  def perform(article)
+    UserMailer.sending_article_to_user(article).deliver_now
+    # "Do something later"
   end
 end
